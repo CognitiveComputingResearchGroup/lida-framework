701e80872646dce1d1626a4dbfe8974562fce2cc
Rodrigo Silva <rsilva@memphis.edu>
2011-02-01 19:37:45 +0000

    Added javadoc comments, todo comments, and fixed formatting.

--- src/edu/memphis/ccrg/lida/sensorymemory/SensoryMemory.java
+++ src/edu/memphis/ccrg/lida/sensorymemory/SensoryMemory.java
@@ -12,20 +12,25 @@ import edu.memphis.ccrg.lida.framework.dao.Saveable;
 import edu.memphis.ccrg.lida.sensorymotormemory.SensoryMotorMemoryListener;
 
 /**
- * Module which senses the environment, stores that data and begins processing it.
+ * This is the interface to be implemented by sensory memory modules. Implementing
+ * modules sense the environment, store the sensed data ,and process it.
  * @author Ryan J. McCall
  */
 //TODO move SensoryMotorMemoryListener to implementation?
 public interface SensoryMemory extends LidaModule, SensoryMotorMemoryListener, Saveable {
 
 	/**
-	 * Module should sense the environment.. 
+	 * Runs all the sensors associated with this memory. The sensors get the
+         * information from the environment and store in this memory for later
+         * processing and passing to the perceptual memory module. 
 	 */
 	public void runSensors();
 	
 	/**
-	 * Add {@link SensoryMemoryListener}
-	 * @param l Listener
+	 * Adds a listener to this memory. This listener constantly
+         * checks for information being sent from this memory to other modules
+         * (Perceptual Associative Memory and Sensory Motor Memory).
+	 * @param l the listener added to this memory
 	 */
 	public void addSensoryMemoryListener(SensoryMemoryListener l);
 
--- src/edu/memphis/ccrg/lida/sensorymemory/SensoryMemoryImpl.java
+++ src/edu/memphis/ccrg/lida/sensorymemory/SensoryMemoryImpl.java
@@ -16,64 +16,103 @@ import edu.memphis.ccrg.lida.framework.LidaModuleImpl;
 import edu.memphis.ccrg.lida.framework.ModuleListener;
 import edu.memphis.ccrg.lida.framework.ModuleName;
 
+/**
+ * This class implements the sensory memory module. This memory's tasks include
+ * sensing the environment, storing the sensed data ,and processing it. This
+ * module sends information to perceptual associative memory, and sensory motor
+ * memory.
+ * @author unascribed
+ */
 public abstract class SensoryMemoryImpl extends LidaModuleImpl implements SensoryMemory {
 
-	private List<SensoryMemoryListener> listeners = new ArrayList<SensoryMemoryListener>();
-	protected Environment environment;
-	protected Object sensoryMemoryContent;
+    /**
+     * The list of listeners associated with this memory, initially PAM & SMM.
+     */
+    private List<SensoryMemoryListener> listeners = new ArrayList<SensoryMemoryListener>();
+    
+    /**
+     * The environment associated with this memory.
+     */
+    protected Environment environment;
+    
+    /**
+     * The content of this memory. 
+     */
+    protected Object sensoryMemoryContent;
 
-	public SensoryMemoryImpl() {
-		super(ModuleName.SensoryMemory);
-	}
+    /**
+     * Constructor of the class.
+     */
+    public SensoryMemoryImpl() {
+        super(ModuleName.SensoryMemory);
+    }
 
-	@Override
-	public void addListener(ModuleListener listener) {
-		if (listener instanceof SensoryMemoryListener) {
-			addSensoryMemoryListener((SensoryMemoryListener) listener);
-		}
-	}
-	
-	@Override
-	public void addSensoryMemoryListener(SensoryMemoryListener l) {
-		listeners.add(l);
-	}
-	
-	@Override
-	public void setAssociatedModule(LidaModule module, int moduleUsage) {
-		if (module != null) {
-			if (module instanceof Environment && 
-				module.getModuleName() == ModuleName.Environment) {
-				environment = (Environment) module;
-			}
-		}
-	}//
-	
-	@Override
-	public void receiveExecutingAlgorithm(Object alg){
-		//What to do here is a research question.
-	}
-	
-	@Override
-	public void decayModule(long ticks) {
-		super.decayModule(ticks);
-		//TODO
-	}
+    @Override
+    public void addListener(ModuleListener listener) {
+        if (listener instanceof SensoryMemoryListener) {
+            addSensoryMemoryListener((SensoryMemoryListener) listener);
+        }
+    }
 
-	//TODO what is this set state garbage? can we delete it all?
-	@Override
-	public boolean setState(Object state) {
+    @Override
+    public void addSensoryMemoryListener(SensoryMemoryListener l) {
+        listeners.add(l);
+    }
+
+    /**
+     * Associates this memory with its respective environment.
+     * @param module the environment associated with this memory
+     * @param moduleUsage not used
+     */
+    @Override
+    public void setAssociatedModule(LidaModule module, int moduleUsage) {
+        if (module != null) {
+            if (module instanceof Environment
+                    && module.getModuleName() == ModuleName.Environment) {
+                environment = (Environment) module;
+            }
+        }
+    }
+
+    /**
+     * The function of this method is still an open question.
+     * @param alg not used
+     */
+    @Override
+    public void receiveExecutingAlgorithm(Object alg) {
+        //TODO What to do here is a research question.
+    }
+
+    @Override
+    public void decayModule(long ticks) {
+        super.decayModule(ticks);
+        //TODO
+    }
+
+    //TODO what are these 2 set/get methods? can we delete them?
+    //Ask Tamas, he implemented the interface.
+    /**
+     * 
+     * @param state
+     * @return
+     */
+    @Override
+    public boolean setState(Object state) {
         try {
             sensoryMemoryContent = state;
             return true;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
-	public Object getState() {
+    public Object getState() {
         return sensoryMemoryContent;
     }
-}// class
+}
