d04480c7752e2420befbb809583732e287fe6f33
Ryan McCall <rmccall@memphis.edu>
2011-07-20 17:50:07 +0000

    update

--- src/alifeagent/environment/operations/AttackOperation.java
+++ src/alifeagent/environment/operations/AttackOperation.java
@@ -13,14 +13,14 @@ import edu.memphis.ccrg.lida.framework.tasks.TaskManager;
 public class AttackOperation implements WorldOperation {
 
 	private static final Logger logger = Logger.getLogger(AttackOperation.class.getCanonicalName());
-	private static final double DEFAULT_ATTACK_HEALTH_DECREASE = -.3;
+	private static final double DEFAULT_ATTACK_HEALTH_DECREASE = -0.3;
 	String soughtObjectName = "agent";
 	
 	@Override
 	public Object performOperation(ALifeWorld world, ALifeObject subject,
 								   ALifeObject[] objects, Object... params) {
 		
-		String soughtObjectName = (String) params[0];
+		soughtObjectName = (String) params[0];
 		Cell currentCell = (Cell) subject.getContainer();
 		Set<ALifeObject> cellObjects = currentCell.getObjects();
 		for(ALifeObject obj: cellObjects){
