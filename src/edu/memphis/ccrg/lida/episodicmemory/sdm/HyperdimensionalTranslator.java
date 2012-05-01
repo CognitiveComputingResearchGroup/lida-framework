1dc14ed33251b81cd3c826b81b0c853513e22e2c
Rodrigo Silva <rsilva@memphis.edu>
2012-05-01 19:52:56 +0000

    Added complement method to BitVectorUtils Added sdm field to HD translator Finished translation methods in HD translator

--- src/edu/memphis/ccrg/lida/episodicmemory/sdm/HyperdimensionalTranslator.java
+++ src/edu/memphis/ccrg/lida/episodicmemory/sdm/HyperdimensionalTranslator.java
@@ -7,21 +7,23 @@
  *******************************************************************************/
 package edu.memphis.ccrg.lida.episodicmemory.sdm;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import cern.colt.bitvector.BitVector;
 import edu.memphis.ccrg.lida.framework.shared.Node;
 import edu.memphis.ccrg.lida.framework.shared.NodeStructure;
+import edu.memphis.ccrg.lida.framework.shared.NodeStructureImpl;
 import edu.memphis.ccrg.lida.framework.shared.Translatable;
 import edu.memphis.ccrg.lida.pam.PamNode;
 import edu.memphis.ccrg.lida.pam.PerceptualAssociativeMemory;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 /**
- * Translates between {@link NodeStructure} objects and {@link BitVector} objects. The methods
- * used are those described by Kanerva in his 2009 hyperdimensional computing
- * paper.
+ * Translates between {@link NodeStructure} objects and {@link BitVector} 
+ * objects. The methods used are those described by Kanerva in his 2009
+ * hyperdimensional computing paper.
  * @author Rodrigo Silva-Lugo
  */
 public class HyperdimensionalTranslator extends BasicTranslator {
@@ -32,31 +34,33 @@ public class HyperdimensionalTranslator extends BasicTranslator {
         
     /** Vector used to map into the sets region of the space. */
     @SuppressWarnings("unused")
-	private BitVector setVector;
-    
+    private BitVector setVector;
+    private SparseDistributedMemory sdm;
+    private static final Logger LOG
+            = Logger.getLogger(HyperdimensionalTranslator.class.getName());    
     private Map<BitVector, Translatable> bvLookUpMap
             = new ConcurrentHashMap<BitVector, Translatable>();
     
     /* TODO: generalize this implementation to translate between representations.
-    Extract interface which encapsulates non-specific respresentations functinality.*/
+    Extract interface which encapsulates non-specific respresentations functionality.*/
 
     /**
      * Constructor of the class.
      * 
-     * @param size
-     *            the number of positions of the bit vector
-     * @param pam
-     *            the PAM associated with this translator
+     * @param size the number of positions in the bit vector
+     * @param pam  the PAM associated with this translator
      */
     public HyperdimensionalTranslator(int size, PerceptualAssociativeMemory pam) {
         super(size, pam);
+        setVector = BitVectorUtils.getRandomVector(size);
     }
     
     /**
      * TODO
-     * @param size the
-     * @param pam the
-     * @param setVector the
+     * @param size      the number of positions in the bit vector
+     * @param pam       the PAM associated with this translator
+     * @param setVector the vector used to map sets into different regions of
+     *                  the space
      */
     public HyperdimensionalTranslator(int size, PerceptualAssociativeMemory pam,
             BitVector setVector) {
@@ -65,31 +69,49 @@ public class HyperdimensionalTranslator extends BasicTranslator {
     }
 
     /**
-     * TODO
-     * @param data the
-     * @return the
+     * Translates a bit vector into a node structure.
+     * @param data  the bit vector to be translated
+     * @return      the resulting node structure
      */
     @Override
     public NodeStructure translate(BitVector data) {
 
-        // 1. Applied inverse mapping
+        //BitVector dataVector = data;
+        NodeStructure structure = new NodeStructureImpl();
+        ArrayList<BitVector> vectors = new ArrayList<BitVector>();
+        // 1. Apply inverse mapping
+        BitVector unmappedSum = BitVectorUtils.multiplyVectors(data, setVector);
         // 2. Cue memory with unmapped sum
+        BitVector probedVector = sdm.retrieve(unmappedSum);
+        while (!probedVector.equals(unmappedSum)) {
+            vectors.add(probedVector);
+            int[] accum = new int[getSize()];
+            BitVectorUtils.getVectorSum(accum, unmappedSum);
+            BitVectorUtils.getVectorSum(
+                    accum, BitVectorUtils.getComplement(probedVector));
+            unmappedSum = BitVectorUtils.getNormalizedVector(accum);
+            probedVector = sdm.retrieve(unmappedSum);
+        }
+        for (BitVector v : vectors) {
+            structure.addDefaultNode((Node) bvLookUpMap.get(v));
+        }
         // 3. Subtract returned vector from sum
         // 4. Cue memory with resulting vector
         // 5. GOTO 2 
-        throw new UnsupportedOperationException("Not supported yet.");
+        return structure;
+        //throw new UnsupportedOperationException("Not supported yet.");
     }
 
     /**
-     * TODO
-     * @param structure the
-     * @return the
+     * Translates a node structure into a bit vector.
+     * @param structure the node structure to be translated
+     * @return          the resulting bit vector
      */
     @Override
     public BitVector translate(NodeStructure structure) {
         Collection<Node> nodes = structure.getNodes();
-        int[] sum = new int[this.getSize()];
-        BitVector result = new BitVector(this.getSize());
+        int[] sum = new int[getSize()];
+        BitVector result;
         for (Node n : nodes) {
             PamNode p = n.getGroundingPamNode();
             BitVector v = p.getSdmId();
